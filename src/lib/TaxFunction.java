a21d4e9fefe323b34ab5b4d021dcf51da17e406a
eliaangga <100867891+eliaangga@users.noreply.github.com>
2023-04-15 23:33:09 +0700

    Refaktor-FunctionPajak--TaxFunction

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,21 +1,20 @@
 package lib;
 
-public class TaxCalculator { 
-'pergantian nama function jadi calculator agar sesuai dengan fungsi dari java tersebut
-karena maenurut sya ini sebenernya untuk perhitungan pajak '
+// public class TaxCalculator { 
+// 'pergantian nama function jadi calculator agar sesuai dengan fungsi dari java tersebut
+// karena maenurut sya ini sebenernya untuk perhitungan pajak '
 
-// /**
-// 	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-// 	 * 
-// 	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-// 	 * 
-// 	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-// 	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-// 	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-// 	 * 
-// 	 */
-	
-	
+private static final int MAX_CHILDREN = 3; // Maksimal jumlah batasannya 3, jika diatas 3 maka akan tertera 3 saja
+private static final int SINGLE_DEDUCTIBLE = 54000000; // ya dengan dengan nilai 54000000, digunakan untuk menghitung penghasilan 
+														// tidak kena pajak bagi pegawai yang belum menikah dan belum memiliki anak.
+private static final int MARRIED_DEDUCTIBLE = 58500000; // dengan nilai 58500000, digunakan untuk menghitung penghasilan tidak kena pajak bagi pegawai yang sudah menikah.
+private static final int CHILD_DEDUCTIBLE = 4500000; // dengan nilai 4500000, digunakan untuk menghitung penghasilan tidak kena pajak 
+													//bagi setiap anak yang dimiliki pegawai, dengan maksimum 3 anak., dan fungsi ini ada ngaruhnya dengan Max_Children 
+													// saat penggunaan
+
+'Menurut saya, sebenernya fungsi ini untuk menunjjukan perubahan pada pada pajak penghasilan didalam Class TaxFunction
+yang menjadi TaxCalculaator'
+
 // 	 public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 // 		int tax = 0;
